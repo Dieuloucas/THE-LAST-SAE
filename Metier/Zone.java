468760d748edebe285950ea38cb2a36e0aa30562
Londhey Manounou <londhey.manounou@etu.univ-lehavre.fr>
2026-06-01 15:24:09 +0200

    Ajout de Zone

---
+++ Metier/Zone.java
@@ -0,0 +1,47 @@
+package metier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zone
+{
+	private int id;
+	private List<Sommet> sommets;
+
+	public Zone(int id)
+	{
+		this.id = id;
+		this.sommets = new ArrayList<>();
+	}
+
+	public int getId() { return id; }
+
+	public void ajouterSommet(Sommet s)
+	{
+		if (!sommets.contains(s))
+		{
+			sommets.add(s);
+			s.setZone(this);
+		}
+	}
+
+	public void retirerSommet(Sommet s)
+	{
+		sommets.remove(s);
+		if (s.getZone() == this)
+		{
+			s.setZone(null);
+		}
+	}
+
+	// Suppression du unmodifiableList
+	public List<Sommet> getSommets()
+	{
+		return sommets;
+	}
+
+	public int getTaille()
+	{
+		return sommets.size();
+	}
+}
