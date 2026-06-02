f77b36a1d8076eae807eaaef6ac6d68a8dd6c428
Londhey Manounou <londhey.manounou@etu.univ-lehavre.fr>
2026-06-02 14:39:03 +0200

    Modification de FrameTransport

--- ihm/FrameTransport.java
+++ ihm/FrameTransport.java
@@ -1,21 +1,40 @@
 package ihm;
 
-import Controleur.*;
-import javax.swing.*;
+import Controleur.Controleur;
 
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
 
 public class FrameTransport extends JFrame
 {
-	private Controleur   ctrl;
-	private PanelCartes  panelCartes;
+	private Controleur ctrl;
+	
 	private PanelPlateau panelPlateau;
-	private PanelEnnemie panelEnnemie;
+	// Décommenter plus tard
+	// private PanelCartes panelCartes;
+	// private PanelEnnemie panelEnnemie;
 
 	public FrameTransport(Controleur ctrl)
 	{
 		this.ctrl = ctrl;
-		
-		this.setSize (700,500           );
+
+
 		this.setTitle("Réseau Express Parisien");
+		this.setSize(800, 600);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new BorderLayout());
+
+		// Instanciation et ajout du plateau au centre
+		this.panelPlateau = new PanelPlateau(this.ctrl);
+		this.add(this.panelPlateau, BorderLayout.CENTER);
+
+		// futurs panels
+		// this.panelCartes = new PanelCartes(this.ctrl);
+		// this.add(this.panelCartes, BorderLayout.SOUTH);
+
+		// this.panelEnnemie = new PanelEnnemie(this.ctrl);
+		// this.add(this.panelEnnemie, BorderLayout.EAST);
+
+		this.setVisible(true);
 	}
 }
